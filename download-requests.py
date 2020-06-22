b56cd39cbf201e35085549c210edce5bd10f620b
Alec Cooper <AlecCooper@users.noreply.github.com>
2020-06-22 18:59:24 -0400

    loops through files

--- download-requests.py
+++ download-requests.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import os
 
 # Given the location of a document, returns a 2d list of original and modern lines
 def parse_document(document):
@@ -29,12 +30,43 @@ def parse_document(document):
 
         # filter out the translated lines
         lines = paragraph.findAll("p", class_ = "speaker-text")
-        #print(lines)
+
         # loop through translated lines 
         for line in lines:
             if line.string != None:
                 translation.append(line.string)
 
-
     return original, translation
     
+# Lists holding the original, translated lines
+original = []
+translation = []
+
+# Directory containing the webpages
+rootdir = os.getcwd() + "/www.litcharts.com/shakescleare/shakespeare-translations"
+
+# We loop through all the directories and parse all the files
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        print("Parsing" + os.path.join(subdir, file))
+        new_original, new_translation = parse_document(os.path.join(subdir, file))
+
+        print(new_original)
+
+        #original = original.extend(new_original)
+        #translation = translation.extend(new_translation)
+        
+# Save to text documents
+with open("original.txt", "w") as f:
+    for line in original:
+        f.write("%s\n" % line)
+
+with open("translation.txt", "w") as f:
+    for line in translation:
+        f.write("%s\n" % line)
+
+
+
+        
+
+
