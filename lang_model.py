15d988d3de33f6a566eca6c93f607dcbfc90a8d5
Alec Cooper <AlecCooper@users.noreply.github.com>
2020-07-07 16:40:01 -0400

    basic structure of the language model

---
+++ lang_model.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+
+class LangModel(Model):
+
+    def __init__(self):
+        super(LangModel, self).__init__()
+
+
+        def encode(self):
+
+            # This method will contain the encoder RNN of the language model
+
+            pass
+
+        
+        def decode(self):
+
+            # This method will contain the decoder RNN of the language model
+
+            pass
+
+        def attention(self):
+
+            # This method will calculate the model's current attention
+
+            pass
