98ef2f20d8434c94905e90b2bdeb8db896d1fe9a
Alec Cooper <AlecCooper@users.noreply.github.com>
2020-07-07 09:57:08 -0400

    file of links we need to download

---
+++ link-parse/link-parser.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+
+# Read in the xml file containing our links
+link_file = open("links.xml","r")
+contents = link_file.read()
+
+# Create our soup
+soup = BeautifulSoup(contents, "xml")
+
+# List will hold our links for output
+link_list = []
+
+# Find every link in the file
+links = soup.findAll("loc")
+
+# Loop through links and add to our lists
+for link in links:
+    link_list.append(link.text)
+
+# Write to file
+with open("links.txt", "w") as f:
+    for link in link_list:
+        f.write("%s\n" % link)
+
