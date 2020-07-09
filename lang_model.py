c13346f617198fabeb6406cf1f01490f6411f992
Alec Cooper <AlecCooper@users.noreply.github.com>
2020-07-09 12:23:53 -0400

    layer params

--- lang_model.py
+++ lang_model.py
@@ -3,11 +3,14 @@ from tensorflow.keras import Model
 
 class Encoder(Model):
 
-    def __init__(self, vocab_len, embed_dim, batch_size):
+    def __init__(self, vocab_len, embed_dim, encode_units, batch_size):
         super(Encoder, self).__init__()
         
         # The size of each batch used when minibatch training
         self.batch_size = batch_size
+
+        # The input dimension to the RNN layers
+        self.encode_units = encode_units
 
         # The embedding layer takes our input,  a vector of positive integers
         # representing indicies and turns them into a dense vector
@@ -15,7 +18,7 @@ class Encoder(Model):
 
         # The lstm layers are the RNNs in our encoder decoder, which are stacked
         # ontop of each other
-        self.lstm = tf.keras.layers.LSTM(self)
+        self.lstm = tf.keras.layers.LSTM(self.encode_units,return_sequences=True,return_state=True)
     
 
     def call(self):
@@ -25,11 +28,14 @@ class Encoder(Model):
 
 class Decoder(Model):
 
-    def __init__(self,vocab_len, embed_dim, batch_size):
-        super(Encoder, self).__init__()
+    def __init__(self,vocab_len, embed_dim, decode_units, batch_size):
+        super(Decoder, self).__init__()
 
         # The size of each batch used when minibatch training
         self.batch_size = batch_size
+
+        # The input dimension to the RNN layers
+        self.decode_units = decode_units
 
         # The embedding layer takes our input,  a vector of positive integers
         # representing indicies and turns them into a dense vector
@@ -37,7 +43,7 @@ class Decoder(Model):
 
         # The lstm layers are the RNNs in our encoder decoder, which are stacked
         # ontop of each other
-        self.lstm = tf.keras.layers.LSTM(self)
+        self.lstm = tf.keras.layers.LSTM(self.decode_units,return_sequences=True,return_state=True)
 
     def call(self):
 
@@ -48,4 +54,3 @@ class Attention(tf.keras.layers.Layer):
     def __init__(self):
         super(Attention, self).__init__()
         
-
