14e626b0b2f9721d7a79150927647a888dfe799f
Alec Cooper <AlecCooper@users.noreply.github.com>
2020-07-09 11:56:06 -0400

    added start of layers

--- lang_model.py
+++ lang_model.py
@@ -3,10 +3,20 @@ from tensorflow.keras import Model
 
 class Encoder(Model):
 
-    def __init__(self):
+    def __init__(self, vocab_len, embed_dim, batch_size):
         super(Encoder, self).__init__()
+        
+        # The size of each batch used when minibatch training
+        self.batch_size = batch_size
 
-   
+        # The embedding layer takes our input,  a vector of positive integers
+        # representing indicies and turns them into a dense vector
+        self.embedding = tf.keras.layers.Embedding(vocab_len, embed_dim)
+
+        # The lstm layers are the RNNs in our encoder decoder, which are stacked
+        # ontop of each other
+        self.lstm = tf.keras.layers.LSTM(self)
+    
 
     def call(self):
 
@@ -15,10 +25,19 @@ class Encoder(Model):
 
 class Decoder(Model):
 
-    def __init__(self):
+    def __init__(self,vocab_len, embed_dim, batch_size):
         super(Encoder, self).__init__()
 
-   
+        # The size of each batch used when minibatch training
+        self.batch_size = batch_size
+
+        # The embedding layer takes our input,  a vector of positive integers
+        # representing indicies and turns them into a dense vector
+        self.embedding = tf.keras.layers.Embedding(vocab_len, embed_dim)
+
+        # The lstm layers are the RNNs in our encoder decoder, which are stacked
+        # ontop of each other
+        self.lstm = tf.keras.layers.LSTM(self)
 
     def call(self):
 
