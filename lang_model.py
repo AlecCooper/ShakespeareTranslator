ff6a9136732b5284fe571e7a434e145d309eca8d
Alec Cooper <AlecCooper@users.noreply.github.com>
2020-10-10 14:54:33 -0700

    Got model training

--- lang_model.py
+++ lang_model.py
@@ -19,22 +19,53 @@ class Encoder(Model):
         # representing indicies and turns them into a dense vector
         self.embedding = tf.keras.layers.Embedding(vocab_len, self.embed_dim, mask_zero=True)
 
-        # The lstm layers are the RNNs in our encoder decoder, which are stacked
-        # ontop of each other
-        self.lstm = tf.keras.layers.LSTM(self.encode_units,return_sequences=True,return_state=True)
+        # The GRU layers
+        self.gru_forward = tf.keras.layers.GRU(self.encode_units,return_sequences=True, recurrent_initializer='glorot_uniform')
+        self.gru_backward = tf.keras.layers.GRU(self.encode_units,return_sequences=True, return_state=True, go_backwards=True, recurrent_initializer='glorot_uniform')
 
-    def call(self, seq_input, mem_state, carry_state):
+    def call(self, seq_input, hidden_state):
 
         # For the first layer we embedd the input into a dense vector
         embed_output = self.embedding(seq_input)
 
-        # 2nd layer, we pass through the lstm
-        seq_output, mem_state, carry_state = self.lstm(embed_output,initial_state=(mem_state, carry_state))
+        # 2nd layer, we pass through the GRUs
+        forward_output = self.gru_forward(embed_output,initial_state=hidden_state)
+        backward_output, hidden_state = self.gru_backward(embed_output,initial_state=hidden_state)
 
-        return seq_output, mem_state, carry_state
+        output = tf.concat([forward_output, backward_output],axis=2)
+
+        return output, hidden_state
 
     def initalize_hidden(self):
-        return (tf.zeros((self.batch_size, self.encode_units)),tf.zeros((self.batch_size, self.encode_units)))
+        return tf.zeros((self.batch_size, self.encode_units))
+
+class Attention(tf.keras.layers.Layer):
+    
+    def __init__(self, units):
+        super(Attention, self).__init__()
+
+        # Layers in the MLP used to calculate score
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, query, values):
+
+        # Brodacast addition over the time axis
+        query = tf.expand_dims(query, 1)
+
+        # Calculate the attention score with a MLP
+        score = self.V(tf.nn.tanh(self.W1(query) + self.W2(values)))
+
+        # Softmax calculates the attention weights
+        attention_weights = tf.nn.softmax(score, axis=1)
+
+        # Calculate the context vector
+        context_vector = attention_weights * values
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        return context_vector, attention_weights
+        
 
 class Decoder(Model):
 
@@ -51,24 +82,37 @@ class Decoder(Model):
         # representing indicies and turns them into a dense vector
         self.embedding = tf.keras.layers.Embedding(vocab_len, embed_dim, mask_zero=True)
 
-        # The lstm layers are the RNNs in our encoder decoder, which are stacked
+        # The GRU layers are the RNNs in our encoder decoder, which are stacked
         # ontop of each other
-        self.lstm = tf.keras.layers.LSTM(self.decode_units,return_sequences=True,return_state=True)
+        self.GRU = tf.keras.layers.GRU(self.decode_units,return_sequences=True,return_state=True)
 
-    def call(self, seq_input, mem_state, carry_state):
+        # Fully connected layer
+        self.fc = tf.keras.layers.Dense(vocab_len)
+
+        # Initalize so we can calculate attention scores and context vectors
+        self.attention = Attention(self.decode_units)
+
+
+    def call(self, seq_input, hidden_state, encoder_output, target_word=None):
+
+        # Create the context vector and weights for the attention mechanism
+        context_vector, attention_weights = self.attention(hidden_state, encoder_output)
 
         # For the first layer we embedd the input into a dense vector
         embed_output = self.embedding(seq_input)
 
-        # 2nd layer, we pass through the lstm
-        seq_output, mem_state, carry_state = self.lstm(embed_output,initial_state=(mem_state, carry_state))
+        concat_output = tf.concat([tf.expand_dims(context_vector, 1), embed_output], axis=-1)
 
-        return seq_output, mem_state, carry_state
+        # 2nd layer, we pass through the GRU
+        seq_output, hidden_state = self.GRU(concat_output,initial_state=hidden_state)
+
+        # Output passed through the fully connected layer
+        output = tf.reshape(seq_output, (-1, seq_output.shape[2]))
+        output = self.fc(output)
+
+        return output, hidden_state, attention_weights
 
     def initalize_hidden(self):
-        return (tf.zeros((self.batch_size, self.encode_units)),tf.zeros((self.batch_size, self.encode_units)))
+        return tf.zeros((self.batch_size, self.encode_units))
 
-class Attention(tf.keras.layers.Layer):
-    def __init__(self):
-        super(Attention, self).__init__()
         
--- train.py
+++ train.py
@@ -22,7 +22,52 @@ def loss_func(actual, pred, loss_obj):
 
     return loss
 
-def train(num_epochs, batch_size, lr, original, translation):
+def train(num_epochs, lr, batch_size, embed_dim, encode_units, dataset):
+
+    # Extract the embedding_dim from the data set
+    vocab_len = 24191     # note: come up with more elegent way to grab this num
+
+    @tf.function
+    def train_step(input_seq, target, encoder_hidden, loss_obj):
+
+        with tf.GradientTape() as tape:
+
+            # Initalize our loss to 0
+            loss = 0
+            
+            # Run the encoder on the input
+            encoder_output, encoder_hidden = encoder(input_seq, encoder_hidden)
+
+            # Initalize first decoder hidden states to final encoder hidden states
+            decoder_hidden = encoder_hidden
+
+            decoder_input = tf.expand_dims(target[:,0],axis=1)
+
+            # Target forcing
+            for t in range(1, target.shape[1]):
+
+                # Pass encoder output into decoder
+                prediction, decoder_hidden, attention_weights = decoder(decoder_input, decoder_hidden, encoder_output)
+                
+                # Calculate loss
+                loss += loss_func(target[:, t], prediction, loss_obj)
+                
+                # Teacher forcing
+                decoder_input = tf.expand_dims(target[:, t], 1)
+
+            # The total loss of the batch
+            batch_loss = (loss / int(target.shape[1]))
+
+            # Train by applying gradients
+            variables = encoder.trainable_variables + decoder.trainable_variables
+            gradients = tape.gradient(loss, variables)
+            optim.apply_gradients(zip(gradients, variables))
+
+        return batch_loss
+
+    # Define the encoder and decoder
+    encoder = Encoder(vocab_len, embed_dim, encode_units, batch_size)
+    decoder = Decoder(vocab_len, embed_dim, encode_units, batch_size)
 
     # Define the optimizer
     optim = tf.keras.optimizers.Adam(learning_rate=lr)
@@ -33,16 +78,22 @@ def train(num_epochs, batch_size, lr, original, translation):
     # Training loop
     for epoch in range(1,num_epochs+1):
 
+        # Initalize the hidden state of our encoder
+        encoder_hidden = encoder.initalize_hidden()
+
+        total_loss = 0
         
         # Loop through our minibatches
         for batch in range(int(len(original)/batch_size)):
 
-            pass
+            input_batch, target_batch = next(iter(dataset))
+            batch_loss = train_step(input_batch, target_batch, encoder_hidden, loss_obj)
+            total_loss += batch_loss
 
-        pass
+            # Verbose output
+            if batch % 100 == 0:
+                print('Epoch {} Batch {} Loss {:.4f}'.format(epoch, batch, batch_loss.numpy()))
 
-    # test loss function
-    #print(loss_func(original, translation, loss_obj))
 
 if __name__ == "__main__":
 
@@ -50,6 +101,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Shakespeare Translator in TensorFlow")
     parser.add_argument("data", metavar="data_dir", help="Folder containing numpy files of original and translated data", type=str)
     parser.add_argument("params",metavar="param_file.json",help="location of hyperparamater json", type=str)
+    parser.add_argument('-c', type=str, default="/training_checkpoints", metavar='/training_checkpoints',help='Enable training checkpoints stored to the given dir')
     args = parser.parse_args()
 
     # Hyperparameters from json file
@@ -68,9 +120,17 @@ if __name__ == "__main__":
     batch_size = hyper["batch size"]
     # Learning rate
     lr = hyper["learning rate"]
+    # Embedding dimension
+    embed_dim = hyper["embedding dimension"]
+    # Numbe of units in our encoder
+    encode_units = hyper["encoder units"]
+
+    # Create Tensorflow dataset
+    dataset = tf.data.Dataset.from_tensor_slices((original, translation))
+    dataset = dataset.batch(batch_size, drop_remainder=True)
 
     # Train the model
-    train(num_epochs, batch_size, lr, original, translation)
+    train(num_epochs, lr, batch_size, embed_dim, encode_units, dataset)
 
     print(np.shape(translation))
     print(np.shape(original))
